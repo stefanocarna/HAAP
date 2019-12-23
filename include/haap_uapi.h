3d7d659817d35ae1b6ec7251af7015a736a0da5e
Stefano Carnà <carna@lockless.it>
2019-12-23 20:24:28 +0100

    Added user_space API to interact with HAAP module

---
+++ include/haap_uapi.h
@@ -0,0 +1,60 @@
+#ifndef _HAAP_UAPI_H
+#define _HAAP_UAPI_H
+
+
+/* Use 'j' as magic number */
+#define HAAP_IOC_MAGIC			'j'
+
+#define _IO_NB	1
+
+#define HAAP_PMC_SETUP			_IO(HAAP_IOC_MAGIC, _IO_NB)
+#define HAAP_PROFILER_ON		_IO(HAAP_IOC_MAGIC, _IO_NB + 1)
+#define HAAP_PROFILER_OFF		_IO(HAAP_IOC_MAGIC, _IO_NB + 2)
+#define HAAP_PMC_RESET			_IO(HAAP_IOC_MAGIC, _IO_NB + 3)
+
+#define HAAP_DATA_READ			_IO(HAAP_IOC_MAGIC, _IO_NB + 5)
+// TODO
+#define HAAP_READ_STATUS		_IO(HAAP_IOC_MAGIC, _IO_NB + 4)
+#define HAAP_RESET_BUFFER		_IO(HAAP_IOC_MAGIC, _IO_NB + 6)
+#define HAAP_SIZE_BUFFER		_IO(HAAP_IOC_MAGIC, _IO_NB + 7)
+
+// Related to context switcher
+#define HAAP_SWITCH_ON			_IO(HAAP_IOC_MAGIC, _IO_NB + 8)
+#define HAAP_SWITCH_OFF			_IO(HAAP_IOC_MAGIC, _IO_NB + 9)
+#define HAAP_PID_ADD			_IO(HAAP_IOC_MAGIC, _IO_NB + 10)
+#define HAAP_PID_DEL			_IO(HAAP_IOC_MAGIC, _IO_NB + 11)
+// TODO 
+#define HAAP_EVT_STATS			_IO(HAAP_IOC_MAGIC, _IO_NB + 12)
+#define HAAP_SIZE_STATS			_IO(HAAP_IOC_MAGIC, _IO_NB + 13)
+#define HAAP_WAIT			_IO(HAAP_IOC_MAGIC, _IO_NB + 14)
+// Re-arrange
+#define HAAP_DEBUG_PRINT		_IO(HAAP_IOC_MAGIC, _IO_NB + 15)
+
+struct pmc_conf {
+	uint64_t event; 
+	uint64_t start;
+	uint64_t reset; 
+	int offcore;
+	int user;
+	int kernel;
+	int pebs;
+	int counting;
+};
+
+struct pmc_conf_list {
+	uint64_t cpu_mask;
+	unsigned size;
+	struct pmc_conf *pmcs;
+};
+
+struct event_stat {
+	uint64_t event;
+	uint64_t value;
+};
+
+struct event_stat_list {
+	unsigned size;
+	struct event_stat *pmcs;
+};
+
+#endif /* _HAAP_UAPI_H */
